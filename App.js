501aec1f4b99ee0c74e22863bf212347e3337063
Navraj singh kaler <navrajkaler996@gmail.com>
2024-05-15 14:09:10 -0500

    linear gradient added

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#0172B2", "#001645"]} style={styles.container}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/RoadmapScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const RoadmapScreen = () => {
+  return <View id="roadmap-container"></View>;
+};
+
+const roadmapStyles = StyleSheet.create({
+  container: {
+    height: "100",
+  },
+});
+
+export default RoadmapScreen;
