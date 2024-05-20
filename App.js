ff9aaa84028651c79100075e5b39aaa6d8207e36
Navraj singh kaler <navrajkaler996@gmail.com>
2024-05-20 01:21:54 -0500

    UI changes

--- App.js
+++ App.js
@@ -5,18 +5,14 @@ import RoadmapScreen from "./screens/RoadmapScreen";
 
 export default function App() {
   return (
-    <LinearGradient colors={["#0172B2", "#001645"]} style={styles.container}>
-      <View>
-        <RoadmapScreen />
-      </View>
-    </LinearGradient>
+    <View style={styles.container}>
+      <RoadmapScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
--- screens/RoadmapScreen.js
+++ screens/RoadmapScreen.js
@@ -5,6 +5,8 @@ import {
   Text,
   View,
   useAnimatedValue,
+  Animated as Animated2,
+  Image,
 } from "react-native";
 
 import Animated, {
@@ -16,82 +18,123 @@ import Animated, {
 
 import { useFonts } from "expo-font";
 
-import AppLoading from "expo-app-loading";
+import { Button, ButtonGroup } from "react-native-elements";
+import { LinearGradient } from "expo-linear-gradient";
 
-const BASE_HEADING_FONT_SIZE = 24;
+import Carousel from "../components/Carousel.js";
+
+const BASE_HEADING_FONT_SIZE = 20;
 const adjustedFontSize = PixelRatio.getFontScale() * BASE_HEADING_FONT_SIZE;
 
 const RoadmapScreen = () => {
   const [fontsLoaded] = useFonts({
-    "font-family-1": require("../assets/fonts/Merriweather-Regular.ttf"),
-    "font-family-2": require("../assets/fonts/Merriweather-Bold.ttf"),
+    "font-family-1": require("../assets/fonts/Poppins-Regular.ttf"),
+    "font-family-2": require("../assets/fonts/Poppins-Bold.ttf"),
+    "font-family-3": require("../assets/fonts/Poppins-Italic.ttf"),
   });
 
-  // if (!fontsLoaded) {
-  //   return <AppLoading />;
-  // }
-
-  const offset1 = useSharedValue(300);
-  const offset2 = useSharedValue(600);
-  const offset3 = useSharedValue(900);
-
-  const animatedText1 = useAnimatedStyle(() => ({
-    transform: [{ translateX: offset1.value }],
-  }));
-  const animatedText2 = useAnimatedStyle(() => ({
-    transform: [{ translateX: offset2.value }],
-  }));
-  const animatedText3 = useAnimatedStyle(() => ({
-    transform: [{ translateX: offset3.value }],
-  }));
-
-  React.useEffect(() => {
-    offset1.value = withRepeat(withTiming(0, { duration: 700 }), 1, true);
-    offset2.value = withRepeat(withTiming(0, { duration: 1400 }), 1, true);
-    offset3.value = withRepeat(withTiming(0, { duration: 2100 }), 1, true);
-  }, []);
   return (
-    <View>
-      <View id="roadmap-container" style={roadmapStyles["text-1-container"]}>
-        <Animated.View style={[animatedText1]}>
-          <Text style={roadmapStyles["text-1"]}>Only roadmap </Text>
-        </Animated.View>
-        <Animated.View style={[animatedText2]}>
-          <Text style={roadmapStyles["text-1"]}>You need to become</Text>
-        </Animated.View>
-        <Animated.View style={[animatedText3]}>
-          <Text style={roadmapStyles["text-1"]}>
-            A fullstack Web developer!
-          </Text>
-        </Animated.View>
-        <View style={roadmapStyles["text-primary-container"]}>
-          <View style={roadmapStyles["text-background"]}>
-            <Text style={roadmapStyles["text-primary"]}>
-              Learning web development is not tough
-            </Text>
-          </View>
-          <View style={roadmapStyles["text-background"]}>
-            <Text style={{ ...roadmapStyles["text-primary"] }}>
-              If you are following a proper roadmap.
-            </Text>
-          </View>
-          <View style={roadmapStyles["text-background"]}>
-            <Text style={{ ...roadmapStyles["text-primary"] }}>
-              We will help you!
-            </Text>
+    <>
+      <LinearGradient
+        colors={["#0172B2", "#001645"]}
+        style={roadmapStyles.container}>
+        <Image
+          source={require("../assets/images/roadmap-logo.png")}
+          style={{
+            width: 200,
+            height: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        />
+        <View style={{ alignItems: "left", marginLeft: 10 }}>
+          <Text style={roadmapStyles["text-1"]}>A proper path</Text>
+          <Text style={roadmapStyles["text-1"]}>A proper skill set</Text>
+          <Text style={roadmapStyles["text-1"]}>A proper web developer</Text>
+        </View>
+        <View
+          id="headings-container-2"
+          style={{
+            flexDirection: "column",
+            alignItems: "flex-end",
+            marginRight: 20,
+          }}>
+          {/* <Text style={roadmapStyles["text-primary"]}>
+            Follow • Learn • Develop
+          </Text> */}
+          <Text style={roadmapStyles["text-primary"]}>Follow</Text>
+          <Text style={roadmapStyles["text-primary"]}>Learn</Text>
+          <Text style={roadmapStyles["text-primary"]}>Develop</Text>
+          {/* <Text style={roadmapStyles["text-secondary"]}>
+            A roadmap helps you learn the right skills in the right order,
+            giving you a clear path from beginner to building your own web apps.
+          </Text> */}
+        </View>
+
+        <View style={{ position: "absolute", bottom: 20 }}>
+          <Carousel />
+        </View>
+      </LinearGradient>
+      <LinearGradient
+        colors={["#fff", "#fff"]}
+        style={roadmapStyles["container-2"]}>
+        <View>
+          {/* <Text style={roadmapStyles["text-primary"]}>
+            Follow • Learn • Develop
+          </Text> */}
+          <View
+            style={{
+              marginTop: 50,
+              // flexDirection: "row",
+              // justifyContent: "center",
+            }}>
+            <Button
+              title="GET STARTED"
+              // type="outline"
+              containerStyle={{ width: "100" }}
+              buttonStyle={{
+                borderColor: "#fff",
+                borderRadius: 60,
+                color: "#fff",
+                paddingRight: 100,
+                paddingLeft: 100,
+                paddingTop: 20,
+                paddingBottom: 20,
+
+                backgroundColor: "#001645",
+                fontFamily: "font-family-2",
+              }}
+            />
           </View>
         </View>
-      </View>
-    </View>
+      </LinearGradient>
+    </>
   );
 };
 
 const roadmapStyles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 0.8,
+    // alignItems: "center",
+    // marginRight: 6,
+    // marginLeft: 6,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    paddingTop: 100,
+    position: "relative",
+  },
+  "container-2": {
+    flex: 0.2,
+    alignItems: "center",
+    marginRight: 6,
+    marginLeft: 6,
+    marginTop: 6,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
   "text-1-container": {
     flex: 1,
     marginTop: 120,
-    // marginLeft: -50,
   },
   "text-1": {
     color: "#fff",
@@ -107,18 +150,42 @@ const roadmapStyles = StyleSheet.create({
   "text-primary": {
     color: "#fff",
     textAlign: "center",
-    fontSize: 16,
+    fontSize: 26,
     letterSpacing: "1",
+    fontFamily: "font-family-2",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  "text-secondary": {
+    color: "#fff",
     fontFamily: "font-family-1",
+    marginLeft: 10,
+    marginRight: 10,
+    textAlign: "center",
   },
   "text-background": {
     backgroundColor: "#001645",
     paddingRight: 40,
     paddingLeft: 40,
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginTop: 5,
-    marginBottom: 5,
+    paddingTop: 30,
+    paddingBottom: 30,
+
+    borderRadius: 20,
+  },
+
+  "button-container": {
+    position: "absolute",
+    bottom: 300,
+    left: "auto",
+    right: "auto",
+  },
+  "images-container-1": {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  "image-styles": {
+    width: 60,
+    height: 60,
   },
 });
 
