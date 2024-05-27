4d6eed9b8c3118b1e8b4c29ad783a39c9cd58b11
Navraj singh kaler <navrajkaler996@gmail.com>
2024-05-26 23:26:57 -0500

    details added

---
+++ data/data.js
@@ -0,0 +1,36 @@
+export const topics = [
+  {
+    id: 0,
+    title: "Basics of web",
+    description:
+      "Before learning web development i.e. developing websites or web applications, you need to understand what is web and how it works.",
+    started: true,
+    completed: false,
+    totalTopics: 16,
+    essentialTopics: 7,
+  },
+  {
+    id: 1,
+    title: "HTML",
+    started: false,
+    completed: false,
+    totalTopics: 13,
+    essentialTopics: 3,
+  },
+  {
+    id: 2,
+    title: "CSS",
+    started: false,
+    completed: false,
+    totalTopics: 32,
+    essentialTopics: 5,
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    started: false,
+    completed: false,
+    totalTopics: 33,
+    essentialTopics: 8,
+  },
+];
--- screens/RoadmapScreen.js
+++ screens/RoadmapScreen.js
@@ -5,6 +5,36 @@ import { AnimatedCircularProgress } from "react-native-circular-progress";
 import CircularProgress from "../components/CircularProgress";
 import { useFonts } from "expo-font";
 import ProgressStepBar from "../components/ProgressStepBar";
+import Steps from "../components/Steps";
+
+const list = [
+  {
+    id: 0,
+    title: "Basics of web",
+    description:
+      "Before learning web development i.e. developing websites or web applications, you need to understand what is web and how it works.",
+    started: true,
+    completed: false,
+    totalTopics: 21,
+    essentialTopics: 7,
+  },
+  {
+    id: 1,
+    title: "HTML",
+    started: false,
+    completed: false,
+    totalTopics: 21,
+    essentialTopics: 7,
+  },
+  {
+    id: 2,
+    title: "CSS",
+    started: false,
+    completed: false,
+    totalTopics: 21,
+    essentialTopics: 7,
+  },
+];
 
 const RoadmapScreen = () => {
   const [fontsLoaded] = useFonts({
@@ -12,15 +42,25 @@ const RoadmapScreen = () => {
     "font-family-2": require("../assets/fonts/Jost-Bold.ttf"),
     "font-family-3": require("../assets/fonts/Jost-Italic.ttf"),
     "font-family-4": require("../assets/fonts/Jost-SemiBold.ttf"),
-    "Lato-regular": require("../assets/fonts/Lato-Black.ttf"),
+    "lato-regular": require("../assets/fonts/Lato-Regular.ttf"),
+    "lato-black": require("../assets/fonts/Lato-Black.ttf"),
   });
   return (
     <View style={roadmapStyles.conatiner}>
-      <View id="progress" style={roadmapStyles["progress-container"]}>
+      <View
+        id="progress"
+        style={{
+          ...roadmapStyles["progress-container"],
+          ...roadmapStyles["resusable-conatiner"],
+          ...roadmapStyles["shadow-1"],
+        }}>
         <CircularProgress />
-
         <ProgressStepBar />
       </View>
+
+      {list.map((item, i) => {
+        return <Steps item={item} index={i} length={list.length} />;
+      })}
     </View>
   );
 };
@@ -30,20 +70,57 @@ const roadmapStyles = StyleSheet.create({
 
   "progress-container": {
     marginTop: 80,
+    marginBottom: 30,
     marginLeft: 10,
     marginRight: 10,
     flexDirection: "row",
     alignItems: "flex-end",
-    backgroundColor: "#fff",
     paddingTop: 20,
     paddingBottom: 20,
     paddingLeft: 10,
     paddingRight: 10,
+  },
+
+  "resusable-conatiner": {
+    backgroundColor: "#fff",
+
     borderRadius: 0,
-    shadowColor: "#171717",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
+  },
+
+  "steps-container": {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    borderRadius: "50%",
+    height: 50,
+    flex: 1,
+    marginLeft: 20,
+  },
+  "steps-circle": {
+    backgroundColor: "#fff",
+
+    borderRadius: "50%",
+    width: 50,
+    height: 50,
+  },
+
+  "progress-line": {
+    height: 60,
+    width: 4,
+    backgroundColor: "gray",
+    marginLeft: 32,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
+  "shadow-1": {
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 2.54,
+    elevation: 3,
   },
 });
 
